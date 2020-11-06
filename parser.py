ab59f420e1b3d2d9656d99456321a446ee3175ed
Yao Yao <yaoy.cs@gmail.com>
2020-11-06 15:18:56 -0800

    moved the code from MyChemInfoClient.py to parser.py; re-organized the code in parser.py; updated the manifest.json accordingly

--- parser.py
+++ parser.py
@@ -1,9 +1,11 @@
 import os
+from collections import ChainMap
 import pandas as pd
-from MyChemInfoClient import batch_query_drugbank_names
+import requests
+
 
 """
-This script parses entries of the repoDB csv file (named `full.csv`) and 
+This script parses entries of the repoDB csv file (named `full.csv`) and
 outputs documents as SmartAPI requires.
 
 Each document represents a unique drug using drugbank id as primary key (`_id`).
@@ -49,6 +51,212 @@ The expected output is:
 ```
 """
 
+############################################
+# Part 1 - Client to query mychem.info API #
+############################################
+
+def query_drugbank_name(drugbank_id):
+    """
+    Find the drugbank name of the input `drugbank_id`
+    See https://docs.mychem.info/en/latest/doc/chem_annotation_service.html#get-request for the specification of the API
+
+    Args:
+        drugbank_id (str): a drugbank ID like "DB00002"
+
+    Returns:
+        drugbank_name (str): a drugbank name like 'Cetuximab' or None if not found
+    """
+    url = 'http://mychem.info/v1/chem/{}'.format(drugbank_id)
+    params = {"fields": "drugbank.name"}
+    response = requests.get(url, params=params)  # GET method required
+
+    # raise an HTTPError if the HTTP request returned an unsuccessful status code
+    response.raise_for_status()
+
+    json_response = response.json()
+
+    """
+    E.g. the json response of http://mychem.info/v1/chem/DB00002?fields=drugbank.name is
+
+    ```
+    {
+        "_id": "DB00002",
+        "_version": 1,
+        "drugbank": {"_license": "http://bit.ly/2PSfZTD", "name": "Cetuximab"}
+    }
+    ```
+
+    The json response of http://mychem.info/v1/chem/DB12430?fields=drugbank.name is
+
+    ```
+    {"code": 404, "success": false, "error": "ID 'DB12430' not found"}
+    ```
+    """
+    if "drugbank" not in json_response:
+        return None
+
+    drugbank_name = json_response["drugbank"]["name"]
+    return drugbank_name
+
+def _query_drugbank_names(drugbank_ids):
+    """
+    Find the drugbank names of the input `drugbank_ids`
+    See https://docs.mychem.info/en/latest/doc/chem_annotation_service.html#batch-queries-via-post for the specification of the API
+    This is a private function that ignores the payload limit (1000 IDs at most in one query)
+
+    Args:
+        drugbank_ids (list): a list of drugbank IDs like ["DB00002", "DB01234"]
+
+    Returns:
+        id_name_map (dict): a mapping of drugbank ids and names like {'DB00002': 'Cetuximab', 'DB01234': 'Dexamethasone'}
+    """
+    url = 'http://mychem.info/v1/chem'
+    data = {"ids": ",".join(drugbank_ids), "fields": "drugbank.name"}
+    response = requests.post(url, data=data)  # POST method required
+
+    # raise an HTTPError if the HTTP request returned an unsuccessful status code
+    response.raise_for_status()
+
+    json_response = response.json()
+
+    """
+    E.g. the json response when querying `["DB00002", "DB12430"]` is
+
+    ```
+    [
+        {
+            'query': 'DB00002',
+            '_id': 'DB00002',
+            '_version': 1,
+            'drugbank': {'_license': 'http://bit.ly/2PSfZTD', 'name': 'Cetuximab'}
+        },
+        {
+            'query': 'DB12430',
+            'notfound': True
+        }
+    ]
+    ```
+    """
+    id_name_map = {entry["query"]: (entry["drugbank"]["name"] if "drugbank" in entry else None) 
+                   for entry in json_response}
+
+    return id_name_map
+
+def batch_query_drugbank_names(drugbank_ids, batch_size=1000):
+    """
+    Find the drugbank names of the input `drugbank_ids` by batches.
+    See https://docs.mychem.info/en/latest/doc/chem_annotation_service.html#batch-queries-via-post for the specification of the API
+    The API has a payload limit that at most 1000 IDs can be posted in one query.
+    Therefore if `len(drugbank_ids)` is greater than 1000, this function will partition the ID list into batches, 
+    call the API multiple times, and merge the results.
+
+    Args:
+        drugbank_ids (list): a list of drugbank IDs like ["DB00002", "DB01234"]
+
+    Returns:
+        id_name_map (dict): a mapping of drugbank ids and names like {'DB00002': 'Cetuximab', 'DB01234': 'Dexamethasone'}
+    """
+    if batch_size is None or len(drugbank_ids) <= batch_size:
+        return _query_drugbank_names(drugbank_ids)
+
+    drugbank_id_batches = [drugbank_ids[i:i+batch_size] for i in range(0, len(drugbank_ids), batch_size)]
+    id_name_map_batches = [_query_drugbank_names(id_batch) for id_batch in drugbank_id_batches]
+
+    id_name_map = dict(ChainMap(*id_name_map_batches))
+    return id_name_map
+
+##########################################
+# Part 2 - Util to revise the repoDB csv #
+##########################################
+
+def revise_drugbank_name(repoDB_df):
+    """
+    Revise the drugbank name of the original repoDB csv.
+
+    Args:
+        repoDB_df (pandas.DataFrame): the original dataframe from the repoDB csv
+
+    Returns:
+        repoDB_df (pandas.DataFrame): the revised dataframe
+    """
+
+    """
+    Addendum (2020-11-05): we found a possible glitch in the original repoDB csv file that one drugbank ID
+    may correspond to multiple drug names. E.g. drugbank ID "DB00002" is found in the following entries:
+
+    | drug_name               | drugbank_id | ind_name                           | ind_id   | NCT         | status     | phase           | DetailedStatus      |
+    |-------------------------|-------------|------------------------------------|----------|-------------|------------|-----------------|---------------------|
+    | cetuximab               | DB00002     | Squamous cell carcinoma of mouth   | C0585362 | NA          | Approved   | NA              | NA                  |
+    | cetuximab               | DB00002     | Squamous cell carcinoma of nose    | C3163899 | NA          | Approved   | NA              | NA                  |
+    | cetuximab               | DB00002     | Squamous cell carcinoma of pharynx | C1319317 | NA          | Approved   | NA              | NA                  |
+    | cetuximab               | DB00002     | Laryngeal Squamous Cell Carcinoma  | C0280324 | NA          | Approved   | NA              | NA                  |
+    | cetuximab               | DB00002     | Malignant tumor of colon           | C0007102 | NA          | Approved   | NA              | NA                  |
+    | NA                      | DB00002     | Non-Small Cell Lung Carcinoma      | C0007131 | NCT00203931 | Terminated | Phase 2         | Slow accrual and ...|
+    | dexamethasone phosphate | DB00002     | Multiple Myeloma                   | C0026764 | NCT00368121 | Terminated | Phase 2         | Lack of ...         |
+    | cetuximab               | DB00002     | Non-Small Cell Lung Carcinoma      | C0007131 | NCT00694603 | Terminated | Phase 2         | Slow accrual ...    |
+    | cetuximab               | DB00002     | Squamous cell carcinoma            | C0007137 | NCT01794845 | Terminated | Phase 2         | Early termination...|
+    | NA                      | DB00002     | Squamous cell carcinoma            | C0007137 | NCT02298595 | Withdrawn  | Phase 1/Phase 2 | NA                  |
+
+    It's not clear how NA was produced in the 1st column but for the 7th entry (dexamethasone phosphate) 
+    Kevin found it's actually a combo drug with cetuximab.
+
+    To maintain the one-to-one relationship between `drug_name` and `drugbank_id`, here we apply the following policy:
+
+    - Discard the original `drug_name` column in the repoDB csv file
+    - Use mychem.info API to find a correct drug name for each drugbank ID
+    """
+
+    """
+    Addendum (2020-11-06): we found that some drug bank IDs are not found in mychem.info (e.g. "DB12430")
+
+    To maintain the one-to-one relationship between `drug_name` and `drugbank_id`, here we apply the following policy:
+
+    - Temporarily discard the original `drug_name` column in the repoDB csv file
+    - Use mychem.info API to find a drug name for each drugbank ID
+        - If not found, use the original `drug_name`
+            - If the original `drug_name` is NA or not unique, manipulate manually
+    """
+
+    drugbank_ids = repoDB_df.drugbank_id.unique()
+    id_name_map = batch_query_drugbank_names(drugbank_ids)
+
+    for _, row in repoDB_df.iterrows():
+        if row.drugbank_id in id_name_map:
+            row.drug_name = id_name_map[row.drugbank_id]
+
+    assert is_one_to_one(repoDB_df, "drug_name", "drugbank_id"), "drug_name and drugbank_id are not 1-to-1 after manipulation"
+
+    return repoDB_df
+
+def is_one_to_one(df, col1, col2):
+    """
+    Check if two columns are one-to-one in a dataframe.
+
+    A one-to-one relation between two columns is like (same with the bijection in functions):
+
+    | col1 | col2      |
+    |------|-----------|
+    | a    | apple     |
+    | b    | banana    |
+    | c    | cranberry |
+
+    Args:
+        df (pandas.DataFrame): a pandas dataframe
+        col1 (str): name of the 1st column
+        col2 (str): name of the 2nd column
+    Returns:
+        result (boolean): True if one-to-one otherwise False
+    """
+    df = df.drop_duplicates(subset=[col1, col2])
+    is_injective = (df.groupby(col1)[col2].count().max() == 1)
+    is_surjective = (df.groupby(col2)[col1].count().max() == 1)
+
+    return is_injective and is_surjective
+
+#####################################
+# Part 3 - Parser of the repoDB csv #
+#####################################
+
 class IndicationEntry:
     def __init__(self, series):
         """Init an indication entry from a pandas Series object."""
@@ -92,92 +300,6 @@ class RepoDBDoc:
         }
 
         return ret_dict
-
-def revise_drugbank_name(repoDB_df):
-    """
-    Revise the drugbank name of the original repoDB csv.
-
-    Args:
-        repoDB_df (pandas.DataFrame): the original dataframe from the repoDB csv
-
-    Returns:
-        repoDB_df (pandas.DataFrame): the revised dataframe
-    """
-
-    """
-    Addendum (2020-11-05): we found a possible glitch in the original repoDB csv file that one drugbank ID
-    may correspond to multiple drug names. E.g. drugbank ID "DB00002" is found on the following entries:
-
-    | drug_name               | drugbank_id | ind_name                           | ind_id   | NCT         | status     | phase           | DetailedStatus                   |
-    |-------------------------|-------------|------------------------------------|----------|-------------|------------|-----------------|----------------------------------|
-    | cetuximab               | DB00002     | Squamous cell carcinoma of mouth   | C0585362 | NA          | Approved   | NA              | NA                               |
-    | cetuximab               | DB00002     | Squamous cell carcinoma of nose    | C3163899 | NA          | Approved   | NA              | NA                               |
-    | cetuximab               | DB00002     | Squamous cell carcinoma of pharynx | C1319317 | NA          | Approved   | NA              | NA                               |
-    | cetuximab               | DB00002     | Laryngeal Squamous Cell Carcinoma  | C0280324 | NA          | Approved   | NA              | NA                               |
-    | cetuximab               | DB00002     | Malignant tumor of colon           | C0007102 | NA          | Approved   | NA              | NA                               |
-    | NA                      | DB00002     | Non-Small Cell Lung Carcinoma      | C0007131 | NCT00203931 | Terminated | Phase 2         | Slow accrual and evidence...     |
-    | dexamethasone phosphate | DB00002     | Multiple Myeloma                   | C0026764 | NCT00368121 | Terminated | Phase 2         | Lack of recruitable patients     |
-    | cetuximab               | DB00002     | Non-Small Cell Lung Carcinoma      | C0007131 | NCT00694603 | Terminated | Phase 2         | Slow accrual                     |
-    | cetuximab               | DB00002     | Squamous cell carcinoma            | C0007137 | NCT01794845 | Terminated | Phase 2         | Early termination due to lack... |
-    | NA                      | DB00002     | Squamous cell carcinoma            | C0007137 | NCT02298595 | Withdrawn  | Phase 1/Phase 2 | NA                               |
-
-    It's not clear how NA was produced in the 1st column but for the 7th entry (dexamethasone phosphate) 
-    Kevin found it's actually a combo drug with cetuximab. 
-
-    To maintain the one-to-one relationship between `drug_name` and `drugbank_id`, here we apply the following policy:
-
-    - Discard the original `drug_name` column in the repoDB csv file
-    - Use mychem.info API to find a correct drug name for each drugbank ID
-    """
-
-    """
-    Addendum (2020-11-06): we found that some drug bank IDs are not found in mychem.info (e.g. "DB12430")
-
-    To maintain the one-to-one relationship between `drug_name` and `drugbank_id`, here we apply the following policy:
-
-    - Temporarily discard the original `drug_name` column in the repoDB csv file
-    - Use mychem.info API to find a drug name for each drugbank ID
-        - If not found, use the original `drug_name`
-            - If the original `drug_name` is NA or not unique, manipulate manually
-    """
-
-    drugbank_ids = repoDB_df.drugbank_id.unique()
-    id_name_map = batch_query_drugbank_names(drugbank_ids)
-
-    for _, row in repoDB_df.iterrows():
-        if row.drugbank_id in id_name_map:
-            row.drug_name = id_name_map[row.drugbank_id]
-
-    assert is_one_to_one(repoDB_df, "drug_name", "drugbank_id"), "drugname and drugbank_id are not 1-to-1 after manipulation"
-
-    return repoDB_df
-
-def is_one_to_one(df, col1, col2):
-    """
-    Check if two columns are one-to-one in a dataframe.
-
-    A one-to-one relation between two columns is like:
-
-    | col1 | col2      |
-    |------|-----------|
-    | a    | apple     |
-    | b    | banana    |
-    | c    | cranberry |
-
-    Same with the bijection in functions.
-
-    Args:
-        df (pandas.DataFrame): a pandas dataframe
-        col1 (str): name of the 1st column
-        col2 (str): name of the 2nd column
-    Returns:
-        result (boolean): True if one-to-one otherwise False
-    """
-    df = df.drop_duplicates(subset=[col1, col2])
-    is_injective = (df.groupby(col1)[col2].count().max() == 1)
-    is_surjective = (df.groupby(col2)[col1].count().max() == 1)
-
-    return is_injective and is_surjective
 
 def load_data(data_folder):
     repoDB_file = os.path.join(data_folder, "full.csv")
